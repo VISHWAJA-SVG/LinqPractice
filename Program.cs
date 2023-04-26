1eb3db819b9365db7b3b83b4d6e9f5229c76826a
VSANJEKA <VSANJEKA@INLEN8520076178>
2023-04-26 15:22:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                 1,2,3,4,5,6,7,8,9,10
+            };
+            var QuerySyntax = from obj in integerList
+                              where obj > 5
+                              select obj;
+            foreach (var item in QuerySyntax)
+            { 
+                Console.WriteLine(item+" ");
+            };
+            Console.ReadKey();
+        }
+    }
+}
